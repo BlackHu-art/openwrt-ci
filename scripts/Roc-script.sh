cd4a54f61251ff8b921673be9453b7c99bed1085
frankie <572890907@qq.com>
2025-08-24 16:27:19 +0800

    remove

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -3,17 +3,6 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Build by OpenWrt')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
-# 升级AriaNG到v1.3.11
-sed -i -e 's/PKG_VERSION:=1.3.10/PKG_VERSION:=1.3.11/' \
-       -e 's/PKG_HASH:=5b76f02ff208b8c948bf8b614511687b7e6d1562323b29494528d89c032fe086/PKG_HASH:=deaaebaf8d59901f0fbdb839daceb1f2768b3d65425e393202786fa2c804bcf9/' \
-          feeds/packages/net/ariang/Makefile
-
-# 升级frp到v0.63.0
-sed -i -e 's/^PKG_VERSION:=0\.51\.3$/PKG_VERSION:=0.63.0/' \
-       -e 's/^PKG_HASH:=83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef$/PKG_HASH:=e5269cf3d545a90fe3773dd39abe6eb8511f02c1dc0cdf759a65d1e776dc1520/' \
-       -e 's#\(\$(INSTALL_DATA) \$(PKG_BUILD_DIR)/\)conf/\(\$(2)\)_full.ini#\1conf/legacy/\2_legacy_full.ini#' \
-       -e '/\$(INSTALL_DATA) \$(PKG_BUILD_DIR)\/conf\/legacy\/\$(2)_legacy_full.ini/a \\t$(INSTALL_DATA) $(PKG_BUILD_DIR)/conf/$(2)_full_example.toml $(1)/etc/frp/$(2).d/' \
-          feeds/packages/net/frp/Makefile
 
 # 调整NSS驱动q6_region内存区域预留大小（ipq6018.dtsi默认预留85MB，ipq6018-512m.dtsi默认预留55MB，带WiFi必须至少预留54MB，以下分别是改成预留16MB、32MB、64MB和96MB）
 # sed -i 's/reg = <0x0 0x4ab00000 0x0 0x[0-9a-f]\+>/reg = <0x0 0x4ab00000 0x0 0x01000000>/' target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq6018-512m.dtsi
