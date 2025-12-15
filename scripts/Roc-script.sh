84a5a8bfa6d72c331cff49b5eb29a438f19b1bba
frankie <572890907@qq.com>
2025-12-15 14:34:53 +0800

    移除AdGuardHome和FileBrowser软件包

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -31,10 +31,9 @@ rm -rf feeds/luci/applications/luci-app-frpc
 rm -rf feeds/luci/applications/luci-app-frps
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/net/open-app-filter
-rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/packages/net/ariang
 rm -rf feeds/packages/net/frp
-# rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
 
 
 # Git稀疏克隆，只克隆指定目录到本地
